53d6f57b91bf26fee8d82da77208aae5fbd3520d
Reed Piernock <reed.piernock@georgetown.edu>
2020-04-21 00:44:23 -0400

    split layout into includes

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,19 @@
-module.exports = {
-  dir: {
-    output: "build"
-  }
+module.exports = function( eleventyConfig ) {
+
+  // Liquid options.
+  let liquidJs = require( "liquidjs" );
+  let options = {
+    extname:         ".html",
+    dynamicPartials: true
+  };
+  eleventyConfig.setLibrary( "liquid", liquidJs( options ) );
+
+  // Eleventy options.
+  return {
+    dir: {
+      output:   "build",
+      includes: "_includes",
+      layouts:  "_layouts"
+    }
+  };
 };
