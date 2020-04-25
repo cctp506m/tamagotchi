473bc1ead014e118abf7d73fb3256ad569b13505
Reed Piernock <reed.piernock@georgetown.edu>
2020-04-25 17:00:09 -0400

    add initial css & js build scripts, basic styles

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp   = require( 'gulp' ),
+      babel  = require( 'gulp-babel' ),
+      concat = require( 'gulp-concat' ),
+      sass   = require( 'gulp-sass' );
+
+sass.compiler = require( 'node-sass' );
+
+const cssFiles = [
+  'source/sass/**/*.scss'
+];
+
+const jsFiles = [
+  'source/js/**/*.js'
+];
+
+gulp.task( 'sass', function () {
+  return gulp.src( cssFiles )
+    .pipe( sass( { outputStyle: 'compressed' } ) )
+    .pipe( concat( 'tamagotchi.min.css' ) )
+    .pipe( gulp.dest( 'docs/_build/css' ) );
+} );
+
+gulp.task( 'js', function() {
+  return gulp.src( jsFiles )
+        .pipe( babel( { presets: [ 'minify' ] } ) )
+        .pipe( concat( 'tamagotchi.min.js' ) )
+        .pipe( gulp.dest( 'docs/_build/js' ) );
+
+});
+
+gulp.task( 'default', gulp.series( 'sass', 'js' ) );
